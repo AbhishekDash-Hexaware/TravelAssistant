0cbc1ca9a215f646facf916e3c702b4d303e506b
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-07 17:01:51 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -5,7 +5,7 @@ module.exports={
   'TrainPrompts': function(request,response){
     var stationknowledge=require("../knowledge/station_code");
     console.log("Slot Response Builder");
-    console.log(JSON.stringify(stationknowledge.data));
+    //console.log(JSON.stringify(stationknowledge.data));
 
   }
 }
