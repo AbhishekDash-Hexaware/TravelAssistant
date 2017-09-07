942754e10af6b10ecf5b41946ccaae8bb27db89a
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-07 20:14:24 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -5,7 +5,7 @@ module.exports={
   'TrainPrompts': function(request,response){
     var stationknowledge=require("../knowledge/station_code");
     console.log("Slot Response Builder");
-    if(request.body.originalRequest.data.message.text!=undefined){
+    if(request.body.originalRequest.data.message.text!=null){
     console.log("User Input Slot Value : "+request.body.originalRequest.data.message.text);
   }
 
