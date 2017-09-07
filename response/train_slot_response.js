7583c9ba945553b7a11642c4599a66789a806a0c
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-07 20:29:41 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -6,10 +6,10 @@ module.exports={
     var stationknowledge=require("../knowledge/station_code");
     console.log("Slot Response Builder");
     console.log("checking out type",JSON.stringify(request.body));
-    if(request.body.originalRequest.data.message.text!=undefined||request.body.originalRequest.data.postback.payload!=find_train){
+    if(request.body.originalRequest.data.message.text!=undefined||request.body.originalRequest.data.postback.payload!="find_train"){
     console.log("User Input Slot Value : "+request.body.originalRequest.data.message.text);
     }
-    
+
 
 
   }
