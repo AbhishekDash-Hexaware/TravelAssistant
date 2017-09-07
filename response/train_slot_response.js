7e8bd85614a48f287c13788103a1eaa601a7a53c
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-07 21:40:50 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -12,12 +12,9 @@ module.exports={
 
     console.log("Slot Response Builder");
     //console.log("checking out type",JSON.stringify(request.body));
-    if(request.body.result.contexts[0].name=="findtrainintent_dialog_params_destination"||request.body.result.contexts[0].name=="findtrainintent_dialog_params_source"){
-    console.log("User Input Slot Value : "+request.body.originalRequest.data.message.text);
+    if(request.body.result.contexts[0].name=="findtrainintent_dialog_params_destination"){
+    console.log("User Input Destination Slot Value : "+request.body.originalRequest.data.message.text);
     var matched  =spell.conversationNorm(request.body.originalRequest.data.message.text);
-    //console.log(matched);
-    }
-
     var facebookResponse={
                              "speech": "",
                              "displayText": "",
@@ -34,6 +31,29 @@ module.exports={
                            }
 
    response.send(facebookResponse);
+    }
+    else if(request.body.result.contexts[0].name=="findtrainintent_dialog_params_source"){
+      console.log("User Input Source Slot Value : "+request.body.originalRequest.data.message.text);
+      var matched  =spell.conversationNorm(request.body.originalRequest.data.message.text);
+      var facebookResponse={
+                               "speech": "",
+                               "displayText": "",
+                               "data": {
+                                 "facebook": [
+                                   {
+                                       "text":"These are the stations I found. Select a specific one please.",
+                                       "quick_replies":matched
+                                   }
+                                 ]
+                               },
+                               "contextOut": [],
+                               "source": "DuckDuckGo"
+                             }
+
+     response.send(facebookResponse);
+    }
+
+
 
 
 
