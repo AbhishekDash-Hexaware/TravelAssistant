44bc3cdacd69abd999fa543b95bace8b7510fedb
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-08 15:04:06 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -11,7 +11,7 @@ module.exports={
 
 
     console.log("Slot Response Builder");
-    //console.log(JSON.stringify(request.body));
+    console.log(JSON.stringify(request.body));
     console.log("Current Context : ",JSON.stringify(request.body.result.contexts[0]));
     console.log("Next Context : ",JSON.stringify(request.body.result.contexts[1]));
     console.log("Custom Source Value : "+request.body.result.fulfillment.source);
