9e93c147d9056f646f7d8cb0748ba3b28b59a8c0
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-08 15:15:26 +0530

    Slot Filling

--- response/train_slot_response.js
+++ response/train_slot_response.js
@@ -62,7 +62,7 @@ module.exports={
                                                }
                                              ]
                                            },
-                                           "contextOut": [],
+                                           "contextOut": ["destination"],
                                            "source": "destination"
                                          }
 
