c288265c483091c4bede47d1c15ae4482541480a
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-10 20:04:31 +0530

    Changev2

--- textprocess/jarowrinkler.js
+++ textprocess/jarowrinkler.js
@@ -3,7 +3,7 @@ var station_knowledge=require("../knowledge/station_code");
 module.exports = {
 	conversationNorm: function (station_name) {
 		if(station_name.length<=4){
-			console.log("Possible Station Code : "+station_name);
+			console.log("Possible Station Code less than length 4 : "+station_name);
 			station_name=station_name.toUpperCase()
 			var all_station_names = station_knowledge.data.entries;
 	    var matched_stations=[];
@@ -23,32 +23,9 @@ module.exports = {
 	      // })
 	      // matched_stations.push(element.synonyms[0])
 	    }
-			else{
-				console.log("Possible Station Name : "+station_name);
-				station_name=station_name.toUpperCase()
-				var all_station_names = station_knowledge.data.entries;
-				var matched_stations=[];
-
-				all_station_names.forEach(function(element){
-					if(natural.JaroWinklerDistance(element.synonyms[0], station_name)>=0.85){
-					//console.log(natural.JaroWinklerDistance(element.synonyms[0], station_name),element.synonyms[0],station_name);
-					let payloadtitle = element.synonyms[0].toLowerCase().replace(/\b[a-z]/g,function(f){return f.toUpperCase();});
-
-
-					let payloadtext=element.synonyms[0]+" $";
-					matched_stations.push({
-						"content_type":"text",
-						"title":payloadtitle,
-						"payload":payloadtext
-					})
-					// matched_stations.push(element.synonyms[0])
-				}
-				})
-				return(matched_stations);
-			}
 
 	    })
-			console.log("Returning : "+matched_stations);
+			console.log("Returning : "+matched_stations)
 	    return(matched_stations);
 		}
 		else{
