e082abe49fc8428825c101c557420c5900dd6fea
Pourab Karchaudhuri <36023@Hexaware.com>
2017-09-11 16:31:53 +0530

    Final Changes

--- textprocess/jarowrinkler.js
+++ textprocess/jarowrinkler.js
@@ -45,10 +45,12 @@ module.exports = {
 		    })
 				console.log("before sorting",confidence_score);
 				console.log("before sorting",result);
+
 				for (var i=0;i<confidence_score.length-1;i++){
 					for (var j=1;j<result.length;j++){
+						console.log("over here before if condition",confidence_score[i],confidence_score[i+1])
 						if(confidence_score[i]>confidence_score[i+1]){
-							console.log("over here",confidence_score[i],confidence_score[i+1])
+							console.log("over hereafter condition",confidence_score[i],confidence_score[i+1])
 							var temp = result[i+1];
 							result[i+1]=result[i];
 							result[i]= temp;
